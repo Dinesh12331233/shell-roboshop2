35f674e14cffa2083fd79104d556c1fe3da63f4c
Dinesh Kancherla <dineshkancherla8@gmail.com>
2025-06-23 18:39:50 +0530

    mysql

---
+++ mysql.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source ./common.sh 
+app_name=mysql
+check_root
+
+echo "Please enter root password to setup"
+read -s MYSQL_ROOT_PASSWORD
+
+
+
+dnf install mysql-server -y &>>$LOG_FILE
+VALIDATE $? "installing mysql server"
+
+systemctl enable mysqld &>>$LOG_FILE
+VALIDATE $? "enabling mysql"
+
+systemctl start mysqld &>>$LOG_FILE
+VALIDATE $? "starting mysql" 
+
+mysql_secure_installation --set-root-pass $MYSQL_ROOT_PASSWORD &>>$LOG_FILE
+VALIDATE $? "setting mysql root password" 
+
+
+print_time
